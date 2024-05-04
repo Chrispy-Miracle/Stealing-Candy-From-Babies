8a75965012a9127b386990a414187490c4744797
Christopher Patchett <chrispymiracle@gmail.com>
2024-05-04 17:34:39 -0600

    improved gravity in player fall state

--- src/states/playerStates/PlayerFallState.lua
+++ src/states/playerStates/PlayerFallState.lua
@@ -6,7 +6,7 @@ function PlayerFallState:init(playState)
 
     self.player.isFalling = true
     gSounds['falling']:play()
-    self.player.gravity = 40 - 30 / self.player.balloonsCarried-- * 5
+    self.player.gravity =  80 - self.player.balloonsCarried * 20
         
     self.playState.backgroundScrollX = 0
 end
@@ -22,28 +22,28 @@ function PlayerFallState:update(dt)
     if self.player.y > VIRTUAL_HEIGHT then
         self.player.y = -self.player.height
         self.player.screensFloatedUp = self.player.screensFloatedUp - 1
+        -- if player is nearing the ground
         if self.player.screensFloatedUp < 1 then
             self.playState.background = 1
             self.player.isFloating =  false
-            
+            -- scoot player and background such that player is back on the ground
             Timer.tween(1, {
                 [self.player] = {y = VIRTUAL_HEIGHT / 2},
                 [self.playState] = {backgroundScrollY = BACKGROUND_Y_LOOP_POINT}
             }):finish(function()
                 gSounds['hit-ground']:play()
+                --damage player
                 self.player.health = self.player.health - (10 / self.player.balloonsCarried)
                 self.player.scoreDetails[self.player.level]['Damage Taken'] = self.player.scoreDetails[self.player.level]['Damage Taken'] + (10 / self.player.balloonsCarried)
+                -- go back to player idle state
                 self.player.stateMachine:change('idle')            
             end)
 
         end
     end
-    
-    -- if self.playState.background == 1 and self.player.y == VIRTUAL_HEIGHT * .8  then
 
-    -- end
-
-    self.player.gravity = self.player.balloonsCarried * 10
+    -- update player gravity and background Y scroll
+    self.player.gravity = 80 - self.player.balloonsCarried * 20
     self.playState.backgroundScrollY = (self.player.gravity * dt + self.playState.backgroundScrollY) % BACKGROUND_Y_LOOP_POINT
     
 
@@ -62,6 +62,7 @@ function PlayerFallState:update(dt)
 
     self.player:update(dt)
 end
+
 
 function PlayerFallState:render()
     self.player:render()
